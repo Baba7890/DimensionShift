5c055fab0bda1fe9b1bfc94354ba775fc0fb9567
Timothy Ng <bladiatim@gmail.com>
2020-01-20 21:18:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DimensionShift
+
+Developed with Unreal Engine 4
---
+++ Source/DimensionShift/DimensionShiftGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DimensionShiftGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIMENSIONSHIFT_API ADimensionShiftGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
