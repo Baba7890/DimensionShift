7caa5948497c566df3ca75c67e31317b3c613886
anintellectual <0120382@kdu-online.com>
2020-02-26 01:26:30 +0800

    enemy runs from player

---
+++ Source/DimensionShift/Enemy_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Enemy_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIMENSIONSHIFT_API AEnemy_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
